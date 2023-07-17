540259c6f748b88909111115d20566920d0552e0
Emran Ramezan <Emran@opulence.design>
2023-07-17 15:14:39 +0100

    Adding disclaimer

--- Tests/example.blade.php
+++ Tests/example.blade.php
@@ -5,11 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=￼">
-    <title>￼</title>
+    <title></title>
 </head>
 
-<body>
+@if(true)
+    <p>hello world</p>
+@endif
+<phptag>{{--
 
+--}}</phptag>
+
+<body>
 </body>
 
 </html>
